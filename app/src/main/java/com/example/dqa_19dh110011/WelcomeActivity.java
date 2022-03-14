e5b92c8df56ef02611010fc1ec6553477d0d84f0
Anh Dương Quốc <19dh110011@st.huflit.edu.vn>
2022-03-14 20:51:54 +0700

    Lab1: finish design

---
+++ app/src/main/java/com/example/dqa_19dh110011/WelcomeActivity.java
@@ -0,0 +1,15 @@
+package com.example.dqa_19dh110011;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
