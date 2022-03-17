87c68df9ad6e51e942c1d39b4433fff6c0bac3c1
Anh Dương Quốc <19dh110011@st.huflit.edu.vn>
2022-03-17 15:51:40 +0700

    Còn name fragment

--- app/src/main/java/com/example/dqa_19dh110011/FullNameFragment.java
+++ app/src/main/java/com/example/dqa_19dh110011/FullNameFragment.java
@@ -71,6 +71,7 @@ public class FullNameFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_full_name, container, false);
     }
 
