89f5e49b5899352721a85936b54d3f45bbfaf85d
duongquocanh1148 <duongquocanh1148@gmail.com>
2022-03-21 15:17:31 +0700

    Done Lab1

--- app/src/main/java/com/example/dqa_19dh110011/MainActivity.java
+++ app/src/main/java/com/example/dqa_19dh110011/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.dqa_19dh110011;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    FirebaseAuth mAuth;
+    Button btnSignOut;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnSignOut = findViewById(R.id.button);
+        mAuth = FirebaseAuth.getInstance();
+        btnSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
